198800f6efd26d5276810432b08630fbfeac43fe
Ethan <ethanhchen01@gmail.com>
2024-12-12 23:09:53 -0600

    first commit, setup problem 1

---
+++ Solution.java
@@ -0,0 +1,17 @@
+/*
+ * Algorithm: have 2 variables: 1 variable is the sum of the smallest of the 2 numbers on each line. the other variable is the sum of the largest of the 2 numbers on each line
+ * The answer should be the difference between the 2 numbers
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+ public class Solution {
+    public static void main(String[] args) {
+        //TODO: read input and send resulting data to solution(). Print the result
+    }
+
+    public int solution(String input) {
+        //TODO: parse the input and sum the smallest of each line into 1 variable and sum the largest of each line into another variable. return the difference
+    }
+ }
