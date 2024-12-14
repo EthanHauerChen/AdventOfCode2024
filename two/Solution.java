922c922ce8645c9dbe83152841c24a55d1afc713
Ethan <ethanhchen01@gmail.com>
2024-12-14 03:20:52 -0600

    setup for problem 2

---
+++ two/Solution.java
@@ -0,0 +1,13 @@
+package two;
+
+import parsing.ParseInput;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int solution(String input) {
+        
+    }
+}
